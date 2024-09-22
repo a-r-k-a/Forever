de5ee754b7eaf196b6fb99fa761eec925713cbfd
Arkajyoti Das <jishu.arka007@gmail.com>
2024-09-22 12:07:52 +0530

    Navbar responsive completed

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Collection from "./pages/Collection";
+import Contact from "./pages/Contact";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Orders from "./pages/Orders";
+import PlaceOrder from "./pages/PlaceOrder";
+import Product from "./pages/Product";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    //root container
+    <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} /> 
+        <Route path="/about" element={<About />} />
+        <Route path="/collections" element={<Collection />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/orders" element={<Orders />} />
+        <Route path="/place_order" element={<PlaceOrder />} />
+        <Route path="/product" element={<Product />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default App;
